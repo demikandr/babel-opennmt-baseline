88ed7ccb361c7ced3d9882c7bed067bcff4174ba
Ubuntu <demikandr@DL.d5izpgxijiiedpgogjyl4nx5le.jx.internal.cloudapp.net>
2018-01-15 19:46:38 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python /path/to/OpenNMT-py/translate.py -model w15012018.pt  -src /data/input.txt -output /output/output.txt -replace_unk -verbose
+python /path/to/OpenNMT-py/translate.py -model /path/to/pretrained.pt  -src /data/input.txt -output /output/output.txt -replace_unk -verbose
 
