d34d83b651743e430906c6879279211f5d7a1b00
Ubuntu <demikandr@DL.d5izpgxijiiedpgogjyl4nx5le.jx.internal.cloudapp.net>
2018-01-15 15:54:15 +0000

    another model

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python /path/to/OpenNMT-py/translate.py -model demo-model_acc_46.62_ppl_22.88_e8.pt  -src /data/input.txt -output /output/output.txt -replace_unk -verbose
+python /path/to/OpenNMT-py/translate.py -model w15012018.pt  -src /data/input.txt -output /output/output.txt -replace_unk -verbose
 
